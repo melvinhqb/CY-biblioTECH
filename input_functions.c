097eb5496af100403d49a77ea5e8de71d92b624f
Melvin HQB <melvinhqb@gmail.com>
2022-05-02 12:04:46 +0200

    Update input_functions.c

--- input_functions.c
+++ input_functions.c
@@ -86,8 +86,9 @@ int CompareTableUserLogin(User *tab, char *login, int size){
 
 int MainMenu(){
 
-    int c;
+    int c = 0;
     int check_size = 0;
+
     char choice[3];
 
     do{
@@ -98,15 +99,26 @@ int MainMenu(){
         
         check_size = ReadInput(choice, sizeof(choice));
 
-        c = (int) strtol (choice, NULL, 10);
+        if(check_size != 1){
 
-        if(c != 1 && c != 2 || check_size != 1){
             printf("\nErreur de saisie, veuillez recommencer !\n");
+
+        }
+        else{
+
+            c = (int) strtol (choice, NULL, 10);
+
+            if(c != 1 && c != 2){
+
+                printf("\nErreur de saisie, veuillez recommencer !\n");
+
+            }
+
         }
     
     }while(c != 1 && c != 2 || check_size != 1);
 
-    return c;
+    return c; // Returns 1 or 2
 
 }
 
@@ -117,16 +129,20 @@ void CreateAccount(char *name_file, int *size){
     char login[22];
     char pswrd[22];
     char role[3];
+
     int cmp = 1;
-    int c;
-    int index;
+    int c = 0;
+    int index = 0;
     int check_size = 0;
+
     FILE *file = NULL;
     User *tab = LoadUsers(name_file, *size);
     
+    // Creating the user's login
     do{
 
         printf("\nLogin\n>>>");
+
         check_size = ReadInput(login, sizeof(login));
 
         if(check_size == 1){
@@ -142,8 +158,6 @@ void CreateAccount(char *name_file, int *size){
             }
 
         }
-
-
         else{
 
             printf("\nCe login est trop grand ! (20 caracteres max)\n");
@@ -153,13 +167,18 @@ void CreateAccount(char *name_file, int *size){
     }while(index != -1 || check_size != 1);
 
     check_size = 0;
+
+    // Creating the user's password
     do{
 
         printf("\nMot de passe\n>>>");
+
         check_size = ReadInput(pswrd, sizeof(pswrd));
 
         if(check_size == 1){
+
             DeleteSpaces(pswrd);
+
         }
         else{
 
@@ -173,26 +192,38 @@ void CreateAccount(char *name_file, int *size){
 
     check_size = 0;
 
+    // Creating the user's role
     do{
 
         printf("\nQuel est votre role ?\n");
         printf("1 - Etudiant\n");
         printf("2 - Professeur\n");
         printf(">>>");
-        check_size = ReadInput(role, sizeof(role));
-        c = (int) strtol (role, NULL, 10);
 
-        if(c != 1 && c != 2 || check_size != 1){
+        check_size = ReadInput(role, sizeof(role));
+
+        if(check_size != 1){
 
             printf("\nErreur de saisie, veuillez recommencer !\n");
+
+        }
+        else{
+
+            c = (int) strtol (role, NULL, 10);
+
+            if(c != 1 && c != 2){
+
+                printf("\nErreur de saisie, veuillez recommencer !\n");
+
+            }
 
         }
 
     }while(c != 1 && c != 2 || check_size != 1);
 
-    
+    // Adds the data to a text file
     file = fopen(name_file, "a");
-    fprintf(file, "%s %s %d 0 0 0 0 0 0 0 0 0 0\n", login, pswrd, c);
+    fprintf(file, "%s %s %d 0 0 0 0 0 0 0 0 0 0\n", login, pswrd, c); // The zeros correspond to the locations of the books and their times
     fclose(file);
     *size += 1;
     printf("\nVotre compte est finalise !\n");
@@ -205,11 +236,14 @@ int ConnectAccount(User *tab, int *size){
 
     char login[22];
     char pswrd[22];
+
     int index;
     int cmp;
     int check_size = 0;
 
+    // Check user's login
     do{
+
         printf("\nLogin\n>>>");
         check_size = ReadInput(login, sizeof(login));
 
@@ -218,32 +252,42 @@ int ConnectAccount(User *tab, int *size){
             DeleteSpaces(login);
 
             index = CompareTableUserLogin(tab, login, *size);
+
         }
 
         if(index == -1 || check_size != 1){
+
                 printf("\nCet utilisateur n'existe pas !\n");
+
         }
 
     }while(index == -1 || check_size != 1);
 
+    // Check user's password
     do{
 
         printf("\nMot de passe\n>>>");
         check_size = ReadInput(pswrd, sizeof(pswrd));
 
-        if(check_size == 1){
+        if(check_size != 1){
+            
+            printf("\nMot de passe incorect !\n");
+        }
+        else{
+
             DeleteSpaces(pswrd);
             Encode(pswrd, VIGENERE_KEY);
             cmp = strcmp(pswrd, tab[index].pswrd);
-        }
 
+            if(cmp != 0){
 
-        if(cmp != 0 || check_size != 1){
-            printf("\nMot de passe incorect !\n");
+                printf("\nMot de passe incorect !\n");
+            }
+
         }
 
     }while(cmp != 0 || check_size != 1);
 
-    return index;
+    return index; // Returns the corresponding line in the text file
 
 }
